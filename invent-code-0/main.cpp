def95af44955e98b16322fbba42162b351fe8c7f
Christian Barrera <christian.michael.barrera@gmail.com>
2020-04-06 20:48:48 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -3,5 +3,8 @@
 using namespace std;
 
 int main() {
+  cout << "My name is Christian!" << endl;
+  cout << "I lived in a tent in Alaska last summer." << endl;
+  cout << "I much prefer the cold over the heat." << endl;
   // Write code here.
 }
--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -3,5 +3,7 @@
 using namespace std;
 
 int main() {
-  // Write code here.
+  cout << "Science fact is more interesting that science fiction. "; 
+  cout << "The world does not belong to me, but rather, I belong to the world. ";
+  cout << "We must all band together to combat the loftiest issue of our generation: climate change. ";
 }
